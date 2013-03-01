7fd549762f58833460a4dcc59d3f64e6d70d82f4
neo <neo5simple@gmail.com>
2013-03-01 10:48:34 +0800

    添加 Android WebService 调用方法，添加缺少的 java 基础类；需添加 ksoap2 jar 包支持哦

---
+++ README.md
@@ -0,0 +1,24 @@
+# neo.droid.commons
+
+跟着 org.apache.commons 学的命令规则，不过没有自己的域名，也不是什么组织，  
+索性就用自己的昵称作为顶域了，莫喷～
+
+搞 Android 开发也快一年了，稍稍整理了点 java 以及 Droid 的常用方法，图的是  
+方便以后项目快速开发，当然，如果您觉得有帮助，`clone` 或者 `fork` 斑驳都很  
+欢迎，多多提 issue 哦。
+
+## 注意
+
++ 2013.03.01 ResUtils.java 中添加了一个调用 WebServices 的静态方法，需要  
+依赖 `ksoap2-android-assembly-3.x-jar-with-dependencies.jar` 请访问其官方  
+的页面 [下载](http://code.google.com/p/ksoap2-android/wiki/HowToUse?tm=2)
+
+# neo.java.commons
+
+这个包里面都是纯 Java 的工具包，都是自己写的，一开始就想把 Android 代码和  
+java 代码分开放置，两边修改也相互不影响。说来也惭愧，学会用 Java 一年多了，  
+也就只整理了这么一点东西，程序员的路任重而道远啊。
+
+
+斑驳敬上
+
--- src/neo/droid/commons/ResUtils.java
+++ src/neo/droid/commons/ResUtils.java
@@ -8,6 +8,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -22,6 +23,12 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.MarshalBase64;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+import org.xmlpull.v1.XmlPullParserException;
 
 import android.app.Activity;
 import android.app.ActivityManager;
@@ -594,5 +601,64 @@ public class ResUtils {
 		} else {
 			USER_AGENT = "";
 		}
+	}
+	
+	/**
+	 * 调用 Web Service
+	 * 
+	 * @param wsdlURL
+	 *            wsdl 完整 URL
+	 * @param namespace
+	 *            命名空间
+	 * @param method
+	 *            调用的方法名称
+	 * @param params
+	 *            调用参数
+	 * @param timeout
+	 *            链接最大超时时间
+	 * @param isDotNet
+	 *            是否是 .Net 服务端
+	 * @return
+	 * @throws IOException
+	 * @throws XmlPullParserException
+	 */
+	public static String CallWebService(String wsdlURL, String namespace,
+			String method, Map<String, String> params, int timeout,
+			boolean isDotNet) throws IOException, XmlPullParserException {
+
+		SoapObject soapObject = new SoapObject(namespace, method);
+
+		if (null != params && params.size() > 0) {
+			String key;
+			Set<String> keysSet = params.keySet();
+			Iterator<String> iterator = keysSet.iterator();
+
+			while (iterator.hasNext()) {
+				key = iterator.next();
+				soapObject.addProperty(key, params.get(key));
+			}
+		}
+
+		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+				SoapEnvelope.VER11);
+		envelope.bodyOut = soapObject;
+		envelope.setOutputSoapObject(soapObject);
+		envelope.encodingStyle = "UTF-8";
+
+		if (false != isDotNet) {
+			envelope.dotNet = true;
+		}
+
+		(new MarshalBase64()).register(envelope);
+
+		HttpTransportSE httpTransportSE = new HttpTransportSE(wsdlURL, timeout);
+		httpTransportSE.debug = true;
+
+		httpTransportSE.call(namespace + method, envelope);
+		if (null != envelope.getResponse()) {
+			return envelope.getResponse().toString();
+		}
+
+		return "";
 	}
 }
